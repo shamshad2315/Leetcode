97b280ebbc951f330e0582483b3e7a4604a84b67
Sham <253752963+shamshad2315@users.noreply.github.com>
2026-08-06 18:00:58 +0530

    Time: 1 ms (99.89%) | Memory: 45.4 MB (14.62%) - LeetSync

---
+++ 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.java
@@ -0,0 +1,20 @@
+ class Solution {
+    public int subarraySum(int[] nums) {
+        int n = nums.length;
+
+        // Prefix Sum Array
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int start = Math.max(0, i - nums[i]);
+            ans += prefix[i + 1] - prefix[start];
+        }
+
+        return ans;
+    }
+}
